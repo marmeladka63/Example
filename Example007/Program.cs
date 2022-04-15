f4a688915cb20ba31b963239f0817856b86fd059
marmeladka63 <peshkova-va@yandex.ru>
2022-04-15 18:33:27 +0300

    Сравнение чисел

---
+++ Example007/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 4;
+int c = 45;
+int d = 567;
+int e = 0;
+int max= a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max=");
+Console.Write(max);
