cb03456880dd5b8217a9eff156f42cfa5bd614fe
marmeladka63 <peshkova-va@yandex.ru>
2022-04-15 18:06:46 +0300

    Выбор приветствия

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Как тебя зовут?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "света")
+{
+    Console.WriteLine("Света пойдем пить чай!");
+}
+else
+{
+    Console.Write("Пойдем плавать ");
+    Console.WriteLine(username);
+
+}
