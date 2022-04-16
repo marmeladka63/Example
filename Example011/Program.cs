bc68a560b2fad53174d6804d8447da1938ec5898
marmeladka63 <peshkova-va@yandex.ru>
2022-04-16 19:50:51 +0300

    Работа с индексами в заданных массивах

---
+++ Example011/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array={1,2,4,56,8,4,56,62,3};
+
+int n=array.Length;
+int find = 56;
+
+int index =0;
+
+while (index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
